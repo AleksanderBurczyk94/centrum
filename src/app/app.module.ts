9865dddd9605c21659c522481d9edb8f890594de
aburczyk <aleksander.burczyk@capgemini.com>
2024-03-13 11:16:15 +0100

    test1

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIconModule} from "@angular/material/icon";
+import {WhiteButtonComponent} from "./shered/white-button/white-button.component";
+import {BodyComponent} from "./core/body/body.component";
+import {OrangeButtonComponent} from "./shered/orange-button/orange-button.component";
+import {NavbarComponent} from "./core/navbar/navbar.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    OrangeButtonComponent,
+    BodyComponent,
+    WhiteButtonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    MatIconModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shered/orange-button/orange-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orange-button',
+  templateUrl: './orange-button.component.html',
+  styleUrls: ['./orange-button.component.css']
+})
+export class OrangeButtonComponent {
+
+}
