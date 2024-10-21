c5628145403b7616485ac45b39f448bb6b0ff639
aburczyk <aleksander.burczyk@capgemini.com>
2024-10-21 19:48:32 +0200

    dodano częściowy lazy loading

--- src/app/core/our-center/our-center.component.ts
+++ src/app/core/our-center/our-center.component.ts
@@ -1,28 +1,10 @@
-import {Component, OnInit, Renderer2} from '@angular/core';
-import * as AOS from 'aos';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-our-center',
   templateUrl: './our-center.component.html',
   styleUrl: './our-center.component.css'
 })
-export class OurCenterComponent implements OnInit {
-  constructor(private renderer: Renderer2) {
-  }
+export class OurCenterComponent {
 
-  ngOnInit(): void {
-    AOS.init({
-      offset: 120, // Odległość od dołu ekranu, przy której zaczyna się animacja
-      duration: 800, // Czas trwania animacji
-      easing: 'ease-in-out', // Styl animacji
-      delay: 100, // Opóźnienie animacji
-      once: true, // Animacja uruchamia się za każdym razem, gdy element staje się widoczny
-      mirror: false, // Animacja także przy przewijaniu do góry
-      anchorPlacement: 'top-bottom' // Określa, gdzie animacja powinna się uruchamiać względem widocznego obszaru
-    });
-
-    this.renderer.listen(window, 'load', () => {
-      AOS.refresh();
-    });
-  }
 }
